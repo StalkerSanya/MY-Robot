5c380c1564186054dc40ef20d93ab16a2b6f9226
Sanya <masterradioteck@yandex.ru>
2019-11-24 23:19:50 +0300

    fixed

--- tests/integration_test/scripts/sub.py
+++ tests/integration_test/scripts/sub.py
@@ -6,42 +6,31 @@ from control_msgs.msg import JointControllerState
 
 
 def check_joint5(msg):
-	if abs(msg.set_point-msg.process_value) < 0.01:
-		print('joint 5 passed test')
-	else:
-		print("failed")
+	assert abs(msg.set_point-msg.process_value) < 0.01, "joint 5 failed"
+	print('joint 5 passed test')
 
 
 def check_joint6(msg):
-	if abs(msg.set_point-msg.process_value) < 0.01:
-		print('joint 6 passed test')
-	else:
-		print("failed")
+	assert abs(msg.set_point-msg.process_value) < 0.01, "joint 6 failed"
+	print('joint 6 passed test')
+	
 
 def check_joint7(msg):
-	if abs(msg.set_point-msg.process_value) < 0.01:
-		print('joint 7 passed test')
-	else:
-		print("failed")
+	assert abs(msg.set_point-msg.process_value) < 0.01, "joint 7 failed"
+	print('joint 7 passed test')
 
 def check_joint8(msg):
-	if abs(msg.set_point-msg.process_value) < 0.01:
-		print('joint 8 passed test')
-	else:
-		print("failed")
+	assert abs(msg.set_point-msg.process_value) < 0.01, "joint 8 failed"
+	print('joint 8 passed test')
 
 def my_robot_joint_positions_subscriber():
 	counter = 0
 	rospy.init_node('image_listener')
 
-	#topics for each leg
-
 	wheel1_topic = '/crobot/joint5_position_controller/state'
 	wheel2_topic = '/crobot/joint6_position_controller/state'
 	wheel3_topic = '/crobot/joint7_position_controller/state'
 	wheel4_topic = '/crobot/joint8_position_controller/state'
-
-	#2 subscribers for 2 legs
 
 	rospy.Subscriber(wheel1_topic,JointControllerState,check_joint5)
 	rospy.Subscriber(wheel2_topic,JointControllerState,check_joint6)
